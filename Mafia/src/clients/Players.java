b62afd3011eb2485e53ca727562b453b72894a9b
Farzam <farzamn80@gmail.com>
2016-03-11 08:42:02 -0700

    Added some starter variables for the Player class which will make future developement easier and the ServerClient.java works beautifully git status

--- Mafia/src/clients/Players.java
+++ Mafia/src/clients/Players.java
@@ -1,5 +1,27 @@
 package clients;
 
 public class Players {
-
+	
+	private String pseudonym;
+	private String playerType;
+	private int lynchCount;
+	
+	public Players(){
+		lynchCount = 0;
+	}
+	
+	// Set the players pseudonym 
+	public void setPseudonum(String pseudonym){
+		this.pseudonym = pseudonym;
+	}
+	
+	// Set the players type (for now, just Innocent or Mafioso)
+	public void setPlayerType(String playerType){
+		this.playerType = playerType;
+	}
+	
+	// Increase the lynch vote for the player
+	public void increaseLynchCount(){
+		lynchCount++;
+	}
 }
--- Mafia/src/game_space/GameSpace.java
+++ Mafia/src/game_space/GameSpace.java
@@ -1,14 +1,24 @@
 package game_space;
 
+import java.util.ArrayList;
+
+import clients.Players;
+
 public class GameSpace{
-	private String[] pseudoList;
-	private boolean daytime;
-	private String[] innocentList;
-	private String[] mofioList;
-	private int roundTimer;
+
+	private ArrayList<Players> innocentList;
+	private ArrayList<Players> mafioList;
+	private ArrayList<Players> toLynch;
 	
-	public GameSpace(){
+	private boolean daytime;
+	private int roundTimer;
 		
+	public GameSpace(){
+		innocentList = new ArrayList<>();
+		mafioList = new ArrayList<>();
+		
+		daytime = false;
+		roundTimer = 0;
 	}
 	
 	public void lynchVote(){
