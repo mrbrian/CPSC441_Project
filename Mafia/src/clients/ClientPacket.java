15bc4aade93941906ae06bcd13edd6ee1e2f4ea1
fnoori <fnoori@ucalgary.ca>
2016-03-19 12:21:11 -0600

    Added read/write

--- Mafia/src/clients/ClientPacket.java
+++ Mafia/src/clients/ClientPacket.java
@@ -1,10 +1,8 @@
 package clients;
 
+import java.nio.ByteBuffer;
+
 public class ClientPacket {
-	
-	private byte type;
-	private int dataSize;
-	private byte[] data;
 	
 	private enum PacketType{
 		CreateAccount,
@@ -20,9 +18,36 @@ public class ClientPacket {
 		GetGameStatus
 	}
 	
-	public ClientPacket(byte type, int dataSize, byte[] data){
+	public PacketType type;
+	public int dataSize;
+	public byte[] data;
+	
+	// Class constructor initializing the fields
+	public ClientPacket(PacketType type, int dataSize, byte[] data){
 		this.type = type;
 		this.dataSize = dataSize;
 		this.data = data;
 	}
+	
+	/* Method that takes a ByteBuffer as argument and
+	 * writes to it the fields of this class
+	 * */
+	public void write(ByteBuffer buffer){
+		buffer.putInt(type.ordinal());
+		buffer.putInt(dataSize);
+		buffer.put(data);
+	}
+	
+	/* Method to read from the ByteBuffer
+	 * Return is of type ClientPacket because
+	 * we need to return multiple things
+	 * */
+	public ClientPacket read(ByteBuffer buffer){
+		PacketType typeLocal = PacketType.values()[buffer.getInt()];
+		int sizeLocal = buffer.getInt();
+		byte[] dataBytes = new byte[sizeLocal];
+		buffer.get(dataBytes);
+		
+		return new ClientPacket(typeLocal, sizeLocal, dataBytes);
+	}	
 }
